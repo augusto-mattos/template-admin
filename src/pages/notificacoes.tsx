c6511be6de7921bf9cd30c732c5c101fc60f33c9
augusto-mattos <guto.s.mattos@gmail.com>
2023-06-21 10:24:06 +0200

    login page

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,12 +1,10 @@
 import Layout from '../components/template/Layout'
-import useAppData from '../data/hook/useAppData'
 
 export default function Notificacoes() {
-  const { alternarTema } = useAppData()
-
+  
   return (
     <Layout titulo='Notificacoes' subtitulo='Aqui serão gerenciadas as notificações'>
-        <button onClick={alternarTema}>Alternar tema</button>
+      <h1>Notificações</h1>
     </Layout>
   )
 }
